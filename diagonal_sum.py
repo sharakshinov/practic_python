40fbaccb1726af23ec095933bd69990b0fcfcf62
Denis Sharakshinov <45946746+sharakshinov@users.noreply.github.com>
2022-12-14 20:12:00 +0300

    Delete diagonal_sum.py

--- diagonal_sum.py
+++
@@ -1,7 +0,0 @@
-def diagSum(mat):
-    summ = 0
-    for i in range(len(mat)):
-        summ += mat[i][i]
-        if i != len(mat) - (i + 1):
-            summ += mat[i][len(mat) - (i + 1)]
-    return summ 
